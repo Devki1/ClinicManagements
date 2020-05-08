2f7ade729d15bc482e36a96327bccaad2eb3037b
Devki1 <gdev3195@gmail.com>
2020-05-09 00:01:44 +0530

    ADDED:Method to addAppointment and implementation

--- src/main/java/bridgelabz/service/CliniqueManagementServiceImp.java
+++ src/main/java/bridgelabz/service/CliniqueManagementServiceImp.java
@@ -1,5 +1,6 @@
 package bridgelabz.service;
 
+import bridgelabz.model.Appointment;
 import bridgelabz.model.Doctor;
 import bridgelabz.model.Patient;
 
@@ -9,6 +10,7 @@ import java.util.ArrayList;
 public class CliniqueManagementServiceImp implements CliniqueInterface {
     FileSystem fileSystem = new FileSystem();
 
+    // This method is used to add doctor information
     @Override
     public void addDoctor(Doctor doctor, String filePath) {
         try {
@@ -22,6 +24,7 @@ public class CliniqueManagementServiceImp implements CliniqueInterface {
 
     }
 
+    // This method is used to add patient information
     @Override
     public void addPatient(Patient patient, String filePath) {
         try {
@@ -34,7 +37,7 @@ public class CliniqueManagementServiceImp implements CliniqueInterface {
         }
     }
 
-
+    // This method is used to search doctor by name.
     @Override
     public boolean searchDoctorByName(String doctorName, String filePath) {
         try {
@@ -50,6 +53,7 @@ public class CliniqueManagementServiceImp implements CliniqueInterface {
         return false;
     }
 
+    // This method is used to search doctor by Id.
     @Override
     public boolean searchDoctorById(int doctorId, String doctorfilePath) {
         try {
@@ -65,6 +69,7 @@ public class CliniqueManagementServiceImp implements CliniqueInterface {
         return false;
     }
 
+    // This method is used to find specialist doctor.
     @Override
     public boolean findDoctorSpecialization(String doctorSpecialist, String doctorFilePath) {
         try {
@@ -81,6 +86,17 @@ public class CliniqueManagementServiceImp implements CliniqueInterface {
         return false;
     }
 
+    // This method is used to search popular doctor.
+    @Override
+    public void popularDoctor(Doctor doctor1, String doctorFilePath) throws IOException {
+        ArrayList<Doctor> doctorArrayList = fileSystem.readFileDoctor(doctorFilePath);
+        for (Doctor doctor : doctorArrayList) {
+            if (doctor.getName().equalsIgnoreCase(doctor1.getName()))
+                System.out.println("Popular doctor is : Dr." + doctor.getName());
+        }
+    }
+
+    // This method is used to search doctor availability.
     @Override
     public boolean searchDoctorByAvailability(String doctorAvailability, String doctorfilePath) {
         try {
@@ -96,6 +112,7 @@ public class CliniqueManagementServiceImp implements CliniqueInterface {
         return false;
     }
 
+    // This method is used to search patient by name.
     @Override
     public boolean searchPatientByName(String patientName, String patientFilePath) {
         try {
@@ -111,6 +128,7 @@ public class CliniqueManagementServiceImp implements CliniqueInterface {
         return false;
     }
 
+    // This method is used to search patient by id.
     @Override
     public boolean searchPatientById(int patientId, String patientFilePath) {
         try {
@@ -126,6 +144,7 @@ public class CliniqueManagementServiceImp implements CliniqueInterface {
         return false;
     }
 
+    // This method is used to search patient by mobile number.
     @Override
     public boolean searchPatientByMobileNumber(Long patientMobileNumber, String patientFilePath) {
         try {
@@ -140,4 +159,33 @@ public class CliniqueManagementServiceImp implements CliniqueInterface {
         }
         return false;
     }
+
+    // This method is used to add appointment.
+    @Override
+    public void addAppointment(Appointment appointment, String filePath) {
+        try {
+            ArrayList<Appointment> readData = fileSystem.readFile(filePath);
+            readData.add(appointment);
+            fileSystem.writeFile(readData, filePath);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // This method is used to fixedAppointment.
+    @Override
+    public void fixedAppointment(Appointment appointmentDoctorName, String appointmentFilePath) throws IOException {
+        ArrayList<Appointment> readData = fileSystem.readAppointmentFile(appointmentFilePath);
+        for (Appointment appointmentDoctor : readData) {
+            if (appointmentDoctor.getDoctorName().equals(appointmentDoctor.getDoctorName()))
+                System.out.println("Doctor name is : Dr." + appointmentDoctor.getDoctorName());
+            if (appointmentDoctor.getPatientName().equals(appointmentDoctor.getPatientName())) ;
+            System.out.println("Patient name is : " + appointmentDoctor.getPatientName());
+            if (appointmentDoctor.getDate().equals(appointmentDoctor.getDate())) ;
+            System.out.println("Date is : " + appointmentDoctor.getDate());
+        }
+    }
 }
+
