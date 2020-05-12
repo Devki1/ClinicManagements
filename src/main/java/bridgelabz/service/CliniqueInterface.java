23499a243dadee83f1c5019678c7444e56bc02b1
Devki1 <gdev3195@gmail.com>
2020-05-13 02:01:00 +0530

    REFACTORED:Correct the code and indentation

--- src/main/java/bridgelabz/service/CliniqueInterface.java
+++ src/main/java/bridgelabz/service/CliniqueInterface.java
@@ -5,31 +5,18 @@ import bridgelabz.model.Doctor;
 import bridgelabz.model.Patient;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface CliniqueInterface {
-    public void addDoctor(Doctor doctor, String filePath);
 
-    public void addPatient(Patient patient, String filePath);
+    public <T> String addRecord(T object, Class<?> className);
 
-    boolean searchDoctorByName(String doctorName, String filePath);
+    public <T> int searchRecord(String searchValue, Class<?> className);
 
-    public boolean searchDoctorById(int doctorId, String doctorfilePath);
+    public <T> List<T> readFile(Class<?> target) throws IOException, ClassNotFoundException;
 
-    boolean findDoctorSpecialization(String doctorSpecialist, String doctorFilePath);
-
-    public boolean searchDoctorByAvailability(String doctorAvailability, String doctorfilePath);
-
-    public boolean searchPatientByName(String patientName, String patientFilePath);
-
-    public boolean searchPatientById(int patientId, String patientfilePath);
-
-    public boolean searchPatientByMobileNumber(Long patientMobileNumber, String patientFilePath);
-
-    public void fixedAppointment(Appointment appointmentDoctorName, String appointmentFilePath) throws IOException;
-
-    public void addAppointment(Appointment appointment, String filePath);
-
-    public void popularDoctor(Doctor doctor1, String doctorFilePath) throws IOException;
-
-
+    public <T> void saveRecord(List<T> list) throws IOException;
 }
+
+
+
