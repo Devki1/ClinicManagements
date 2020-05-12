bf82fefa2f19a41813ec88f3f691ead65aec9711
Devki1 <gdev3195@gmail.com>
2020-05-13 01:56:54 +0530

    REFACTORED:Correct indentation

--- src/main/java/bridgelabz/model/Doctor.java
+++ src/main/java/bridgelabz/model/Doctor.java
@@ -1,60 +1,63 @@
 package bridgelabz.model;
 
 public class Doctor {
-    private String name;
-    private int id;
-    private String specialization;
-    private String avl;
+    private String doctor_Id;
+    private String doctor_Name;
+    private String doctor_MobileNumber;
+    private String doctor_Address;
+    private String doctor_Availability;
+    private String doctor_Specialist;
 
-    Doctor() {
+    // Default constructor
+    public Doctor() {
     }
 
-    public Doctor(String name, Integer id, String spl, String avl) {
-        this.setName(name);
-        this.setId(id);
-        this.setSpecialization(spl);
-        this.setAvl(avl);
+
+    // Constructor to assign doctor properties
+    public Doctor(String doctor_Id, String doctor_Name, String doctor_MobileNumber, String doctor_Address,
+                  String doctor_Availability, String doctor_Specialist) {
+        this.doctor_Id = doctor_Id;
+        this.doctor_Name = doctor_Name;
+        this.doctor_Address = doctor_Address;
+        this.doctor_Availability = doctor_Availability;
+        this.doctor_Specialist = doctor_Specialist;
+        this.doctor_MobileNumber = doctor_MobileNumber;
+    }
+    // Getter method
+
+    public String getDoctor_Id() {
+        return doctor_Id;
     }
 
-    public String getName() {
-        return name;
+    public String getDoctor_Name() {
+        return doctor_Name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getDoctor_MobileNumber() {
+        return doctor_MobileNumber;
     }
 
-    public int getId() {
-        return id;
+    public String getDoctor_Address() {
+        return doctor_Address;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public String getDoctor_Availability() {
+        return doctor_Availability;
     }
 
-    public String getSpecialization() {
-        return specialization;
-    }
-
-    public void setSpecialization(String specialization) {
-        this.specialization = specialization;
-    }
-
-    public String getAvl() {
-        return avl;
-    }
-
-    public void setAvl(String avl) {
-        this.avl = avl;
+    public String getDoctor_Specialist() {
+        return doctor_Specialist;
     }
 
     @Override
     public String toString() {
         return "Doctor{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                ", specialization='" + specialization + '\'' +
-                ", avl='" + avl + '\'' + '}';
+                "doctor_Id='" + doctor_Id + '\'' +
+                ", doctor_Name='" + doctor_Name + '\'' +
+                ", doctor_MobileNumber='" + doctor_MobileNumber + '\'' +
+                ", doctor_Address='" + doctor_Address + '\'' +
+                ", doctor_Availability='" + doctor_Availability + '\'' +
+                ", doctor_Specialist='" + doctor_Specialist + '\'' +
+                '}';
     }
 }
-
