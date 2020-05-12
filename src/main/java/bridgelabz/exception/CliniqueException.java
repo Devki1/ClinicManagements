2445c9d97605e312de63c34cdea24769a04bed32
Devki1 <gdev3195@gmail.com>
2020-05-13 01:54:32 +0530

    ADDED:Exception type

---
+++ src/main/java/bridgelabz/exception/CliniqueException.java
@@ -0,0 +1,15 @@
+package bridgelabz.exception;
+
+public class CliniqueException extends Exception {
+    public enum MyException {
+        INVALID_MOBILE_NUMBER, FILE_EMPTY, INVALID_ID, INVALID_APPOINTMENT_DATE
+    }
+
+    MyException type;
+
+    public CliniqueException(MyException type, String message) {
+        super(message);
+        this.type = type;
+    }
+}
+
