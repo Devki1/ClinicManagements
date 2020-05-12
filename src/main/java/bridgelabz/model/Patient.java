82be036fda57db087231c603c0b6808295168293
Devki1 <gdev3195@gmail.com>
2020-05-13 01:57:28 +0530

    REFACTORED:Correct indentation

--- src/main/java/bridgelabz/model/Patient.java
+++ src/main/java/bridgelabz/model/Patient.java
@@ -1,57 +1,48 @@
 package bridgelabz.model;
 
 public class Patient {
-    private String name;
-    private int id;
-    private Long mobileNumber;
-    private int age;
+    private String patientId;
+    private String patientName;
+    private String patientMobileNumber;
+    private int patientAge;
 
-    Patient() {
-
-    }
-
-    public Patient(String name, int id, Long mobileNumber, int age) {
-        this.name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
-        this.id = id;
-        this.mobileNumber = mobileNumber;
-        this.age = age;
-
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Long getMobileNumber() {
-        return mobileNumber;
-    }
-
-    public void setMobileNumber(Long mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+    // Default constructor
+    public Patient() {
     }
 
 
+    // Constructor to assign patient properties
+    public Patient(String patientId, String patientName, String patientMobileNumber, int patientAge) {
+        this.patientId = patientId;
+        this.patientName = patientName;
+        this.patientAge = patientAge;
+        this.patientMobileNumber = patientMobileNumber;
+    }
+
+    // Getter methods
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public String getPatientMobileNumber() {
+        return patientMobileNumber;
+    }
+
+    public int getPatientAge() {
+        return patientAge;
+    }
+
+    @Override
+    public String toString() {
+        return "Patients{" +
+                "patientId='" + patientId + '\'' +
+                ", patientName='" + patientName + '\'' +
+                ", patientMobileNumber='" + patientMobileNumber + '\'' +
+                ", patientAge=" + patientAge +
+                '}';
+    }
 }
-
-
-
